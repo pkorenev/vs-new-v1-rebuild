1030fa0fa811d46a8cf36e0f6ec9fdeb74eb7536
pasha <p.korenev@voroninstudio.eu>
2014-04-23 13:47:53 +0300

    enable static_page_data model to edit

--- app/models/static_page_data.rb
+++ app/models/static_page_data.rb
@@ -1,3 +1,20 @@
 class StaticPageData < ActiveRecord::Base
   belongs_to :has_static_page_data, :polymorphic => true
+
+  attr_accessible :url
+  attr_accessible :body_title
+  attr_accessible :head_title
+  attr_accessible :meta_keywords
+  attr_accessible :meta_description
+
+  attr_accessible :has_static_page_data_id
+  attr_accessible :has_static_page_data_type
+
+  rails_admin do
+    edit do
+      field :head_title
+      field :meta_keywords
+      field :meta_description
+    end
+  end
 end
