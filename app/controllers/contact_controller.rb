9025f1f2a99e123cdbf470018286d9868ad2e7e1
pasha <p.korenev@voroninstudio.eu>
2014-05-21 14:08:44 +0300

    test

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -77,7 +77,7 @@ class ContactController < ApplicationController
     f.text = params['message']
 
     f.save
-    render inline: params.inspect
+    render inline: params['forms_join_us']['portfolio'].inspect
 
     #@hostname = request.inspect
 
