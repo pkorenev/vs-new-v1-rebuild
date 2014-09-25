db7b4f10da2d0aeb5e4b3eb8fbd7dba4f7ed2532
pasha <p.korenev@voroninstudio.eu>
2014-09-25 18:11:04 +0300

    add links for turbolinks

--- app/assets/javascripts/add_links.js
+++ app/assets/javascripts/add_links.js
@@ -1,6 +1,12 @@
+function init_locale_links(){
 $('span.untranslated-locale').each(function(){
     $span = $(this)
     var $li = $span.parent()
     $li.append('<a class="added-via-js" href="'+$span.attr('data-href')+'">'+$span.text()+'</a>')
     $span.remove()
 })
+}
+
+init_locale_links()
+
+$(document).on('page:load', init_locale_links)
