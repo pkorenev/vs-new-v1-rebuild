64b14f03b4e4612b3af701d09bd332caf6f734d6
VoroninNick <p.korenev@voroninstudio.eu>
2015-04-16 17:18:11 +0300

    price

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -5,9 +5,9 @@ class Ckeditor::Picture < Ckeditor::Asset
 
   has_paperclip_attached_file :data,
                     url: "/ckeditor_assets/pictures/:id/:style_:basename.:extension",
-                    path: ":rails_root/public/:url",
-                    styles: proc { |a| a.instance.resolve_data_styles },
-                    processors: [:thumbnail, :paperclip_optimizer]
+                    path: ":rails_root/public/:url"
+                    #styles: proc { |a| a.instance.resolve_data_styles },
+                    #processors: [:thumbnail, :paperclip_optimizer]
 
   validates_attachment_size :data, :less_than => 10.megabytes
   validates_attachment_presence :data
